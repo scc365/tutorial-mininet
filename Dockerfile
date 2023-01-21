acab48f6c092e124333f0719b5231fd832d1e7e0
savakazakov <savakazakov@gmail.com>
2023-01-21 13:10:42 +0000

    Updated topology

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM ghcr.io/scc365/mn:latest
 WORKDIR /topology
 COPY topology.py .
 
-CMD [ "--custom /topology/topology.py --topo tutorialTopology --switch ovsk" ]
+CMD [ "--custom /topology/topology.py --topo tutorialTopology --switch ovsk --controller remote,ip=0.0.0.0,port=6633" ]
--- topology.py
+++ topology.py
@@ -1,17 +1,25 @@
 from mininet.topo import Topo
 
-class TutorialTopology( Topo ):
+class TutorialTopology(Topo):
 
-  def build( self ):
-    # add a host to the network
-    h1 = self.addHost( 'h1' )
+    def build(self):
+        # Sdd a host to the network.
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
 
-    # add a switch to the network
-    s1 = self.addSwitch( 's1' )
+        # Add a switch to the network.
+        s1 = self.addSwitch('s1')
 
-    # add a link between the host `h1` and the `s1` switch
-    self.addLink( h1, s1 )
+        # Add a link between the host 'h1' and the 's1' switch.
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s1)
+        self.addLink(h5, s1)
 
-# the topologies accessible to the mn tool's `--topo` flag
-# note: if using the Dockerfile, this must be the same as in the Dockerfile
-topos = { 'tutorialTopology': ( lambda: TutorialTopology() ) }
+# The topologies accessible to the mn tool's '--topo' flag.
+# note: if using the Dockerfile, this must be the same as in the Dockerfile.
+topos = {'tutorialTopology': (lambda: TutorialTopology())}
