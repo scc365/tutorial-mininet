834ae8df58bdeccdad283125e3488d46a0e3d6d3
savakazakov <savakazakov@gmail.com>
2023-01-22 13:31:12 +0200

    Workshop 1 done

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM ghcr.io/scc365/mn:latest
+ARG topology
+ENV TOPOLOGY ${topology}
+
+RUN echo ${TOPOLOGY}
 
 WORKDIR /topology
 COPY topology.py .
 
-CMD [ "--custom /topology/topology.py --topo tutorialTopology --switch ovsk --controller remote,ip=0.0.0.0,port=6633" ]
+CMD ["sh", "-c", "--custom /topology/topology.py --topo ${TOPOLOGY} --switch ovsk"]
--- topology.py
+++ topology.py
@@ -3,23 +3,58 @@ from mininet.topo import Topo
 class TutorialTopology(Topo):
 
     def build(self):
-        # Sdd a host to the network.
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
+        numOfHosts = 10
+        numOfSwitches = 2
+        hosts = []
+        switches = []
+        
+        # Add hosts to the network.
+        for i in range(numOfHosts):
+            hosts.append(self.addHost("h" + str(i)))
 
         # Add a switch to the network.
-        s1 = self.addSwitch('s1')
+        for i in range(numOfSwitches):
+            switches.append(self.addSwitch("s" + str(i)))
 
-        # Add a link between the host 'h1' and the 's1' switch.
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(h3, s1)
-        self.addLink(h4, s1)
-        self.addLink(h5, s1)
+        # Add a links between the host on the left and switch on the left.
+        for i in range(numOfHosts):
+            if(i < numOfHosts / 2):
+                self.addLink(hosts[i], switches[0])
+            else:
+                self.addLink(hosts[i], switches[1])
+        
+        # Add the link between the switches.
+        self.addLink(switches[0], switches[1])
+        
+class NewTopology(Topo):
+
+    def build(self):
+        numOfHosts = 4
+        numOfSwitches = 2
+        hosts = []
+        switches = []
+        
+        # Add hosts to the network.
+        for i in range(numOfHosts):
+            hosts.append(self.addHost("h" + str(i)))
+
+        # Add a switch to the network.
+        for i in range(numOfSwitches):
+            switches.append(self.addSwitch("s" + str(i)))
+
+        # Add a links between the host on the left and switch on the left.
+        for i in range(numOfHosts):
+            if(i < numOfHosts / 2):
+                self.addLink(hosts[i], switches[0])
+            else:
+                self.addLink(hosts[i], switches[1])
+                
+        # Add the link between the switches.
+        self.addLink(switches[0], switches[1])
 
 # The topologies accessible to the mn tool's '--topo' flag.
 # note: if using the Dockerfile, this must be the same as in the Dockerfile.
-topos = {'tutorialTopology': (lambda: TutorialTopology())}
+topos = {
+            "tutorialTopology": (lambda: TutorialTopology()),
+            "newTopology": (lambda: NewTopology())
+        }
